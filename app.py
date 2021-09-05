f47526e6596565a30140a94e2f73b74500863820
Danielle Myrick <danielle.myrick@gmail.com>
2021-09-05 10:02:00 -0400

    Updated - Customize the Appearance.

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def scrape():
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 if __name__ == "__main__":
-   app.run()
+   app.run(use_debugger=False, use_reloader=False, passthrough_errors=True)
